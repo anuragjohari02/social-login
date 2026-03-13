15dc037f16f07ae9e9667074b266fe6808ac49d3
anuragjohary02 <anuragkumarjohary@gmail.com>
2026-03-13 15:39:54 +0530

    correct the path

--- public/index.php
+++ public/index.php
@@ -8,10 +8,10 @@ session_start();
     <h3>Welcome, <?php echo htmlspecialchars($_SESSION['user']['name']); ?>!</h3>
     <p>Email: <?php echo htmlspecialchars($_SESSION['user']['email']); ?></p>
     <!-- logout button -->
-    <form action="public/logout.php" method="post">
+    <form action="logout.php" method="post">
         <button type="submit" class="logout-btn">Logout</button>
     </form>
 <?php else : ?>
     <p>You are not logged in.</p>
-    <a href="public/login.php">Go to Login</a>
+    <a href="login.php">Go to Login</a>
 <?php endif; ?>
